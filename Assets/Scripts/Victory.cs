1bd148d6e8748fefacecf80ded05d59d9d971f66
Emrik Lind <emrik.lind@elev.ga.lbs.se>
2018-11-13 08:37:03 +0100

    Victory

---
+++ Assets/Scripts/Victory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Victory : MonoBehaviour
+{
+    public string SceneToLoad = "SampleScene";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+    }
+
+
+
+
+
+
+}
