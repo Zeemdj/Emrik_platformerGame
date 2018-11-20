6153d079a2886e6a88978daf2c9d3e9de269e7f0
Emrik Lind <emrik.lind@elev.ga.lbs.se>
2018-11-20 08:16:44 +0100

    Added decorations

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Victory : MonoBehaviour
+public class LoadLevel : MonoBehaviour
 {
     public string SceneToLoad = "SampleScene";
 
